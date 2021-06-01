803514e446ea761681bb8e86962cf7760109d7a7
Enrique Martínez <kikemartinez1996@gmail.com>
2021-06-01 16:34:09 +0200

    admin test

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,10 +1,16 @@
 import { Express } from 'express';
 import { ProductsGetController } from '../controllers/ProductsGetController';
 import { ProductsInsertController } from '../controllers/ProductsInsertController';
+import { AuthValidation } from '../shared/domain/AuthValidation';
+import AuthValidationJWT from '../shared/infrastructure/AuthValidationJWT';
+
 
 export const register = (app: Express) => {
+
+  const verifyAdmin: AuthValidation = new AuthValidationJWT();
+
   const productsGetController: ProductsGetController = new ProductsGetController();
-  app.get('/product', productsGetController.run.bind(productsGetController));
+  app.get('/product', verifyAdmin.verifyAdmin, productsGetController.run.bind(productsGetController));
   
   const productsInsertController: ProductsInsertController = new ProductsInsertController();
   app.get('/product-insert', productsInsertController.run.bind(productsInsertController));
